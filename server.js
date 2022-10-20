76d25fb73acf7c0cf1c13ec198440253ab7c0c64
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-10-20 20:52:36 +0200

    server: print listening port

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const middlewares = jsonServer.defaults({
 server.use(middlewares)
 server.use(router)
 
-server.listen(3000, () => {
-  console.log('JSON Server is running')
+const port = 3000
+server.listen(port, () => {
+  console.log('JSON Server is listening on port', port)
 })
